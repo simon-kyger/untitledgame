e7034b61c7be02c03d64621597895ff0dc9e193b
simon-kyger <simonkyger@hotmail.com>
2020-05-31 15:08:53 -0400

    blur event to stop all current keyboard commands from processing game logic if window is not focused

--- keyboard.js
+++ keyboard.js
@@ -6,13 +6,16 @@ export default GAME => {
             this[key] = newkey 
         else throw Exception(`unkown key ${key}`)
     }
-    window.addEventListener('keydown', e=> {
+    window.addEventListener('keydown', e => {
         const key = Object.values(keyboard).filter(o => o.key == e.key)[0]
         if (key) key.state = true
     })
-    window.addEventListener('keyup', e=> {
+    window.addEventListener('keyup', e => {
         const key = Object.values(keyboard).filter(o => o.key == e.key)[0]
         if (key) key.state = false
+    })
+    window.addEventListener('blur', () => {
+        Object.values(keyboard).filter(o => o.state = false)
     })
     return keyboard
 }
