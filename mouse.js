0abc55aeef87d5b6699851912771889a81ffdcb5
simon-kyger <simonkyger@hotmail.com>
2020-06-09 22:43:04 -0400

    fixed mousemove

--- mouse.js
+++ mouse.js
@@ -4,8 +4,10 @@ export default GAME => {
         const player = GAME.entities.get('player')
         const camera = GAME.entities.get('camera')
         //TODO: this is wrong
-        const x = Math.floor(e.offsetX)
-        const y = Math.floor(e.offsetY)
+        const x = Math.floor(((e.offsetX / GAME.screen.image.style.width.slice(0, -2)) * GAME.resolution.width) + camera.x)
+        const y = Math.floor(((e.offsetY / GAME.screen.image.style.height.slice(0, -2)) * GAME.resolution.height) + camera.y)
+        console.log('e.offsetX', e.offsetX)
+        console.log('e.offsetY', e.offsetY)
         console.log('clicked here: x', x, 'y', y)
         switch (e.button) {
             case 0:
